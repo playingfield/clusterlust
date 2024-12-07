e0c5181283c66eac10b69dd57f411ae74b62e8cd
Bas Meijer <bas.meijer@me.com>
2024-12-07 17:37:10 +0100

    no dependent roles

--- galaxy.sh
+++ galaxy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -rf galaxy_roles/*
-ansible-galaxy install -p galaxy_roles -r roles/requirements.yml
+#rm -rf galaxy_roles/*
+#ansible-galaxy install -p galaxy_roles -r roles/requirements.yml
 ansible-galaxy collection install -r collections/requirements.yml
